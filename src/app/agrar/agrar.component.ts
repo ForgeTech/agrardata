c49ea535b784e3a9501c80d948c844c34d0d215e
FloNeu <di.neumann.florian@gmail.com>
2020-02-03 13:19:17 +0100

    More directive and pipe example

--- src/app/agrar/agrar.component.ts
+++ src/app/agrar/agrar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-agrar',
@@ -9,6 +9,10 @@ export class AgrarComponent implements OnInit {
   // Class-Member variables
   public value: number = 10;
   public showIf: boolean = true;
+  public birthday = new Date(1988, 3, 15); // April 15, 1988
+  @Input() public config: number[] = [ 1, 2, 3 ];
+  @Input() public name: string = 'world';
+
   // Class Constructor
   constructor() {
     console.log('CONSTRUCTOR');
