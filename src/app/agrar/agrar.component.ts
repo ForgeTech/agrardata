78962d93072cc1f5b0ac551da87301f78bb40346
FloNeu <di.neumann.florian@gmail.com>
2020-02-03 12:12:29 +0100

    ng-if and ng-template switcher example

--- src/app/agrar/agrar.component.ts
+++ src/app/agrar/agrar.component.ts
@@ -6,15 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agrar.component.scss']
 })
 export class AgrarComponent implements OnInit {
-
+  // Class-Member variables
   public value: number = 10;
-
+  public showIf: boolean = true;
+  // Class Constructor
   constructor() {
     console.log('CONSTRUCTOR');
   }
-
+  // Angular Live-Cycle Methode
   ngOnInit() {
     console.log('NG INIT');
+  }
+  // Class-Memeber Methode
+  public switchIf( event: Event): void {
+    event.preventDefault();
+    this.showIf = !this.showIf;
   }
 
 }
