bb85f5f4369fd035dddf5d2829fb70418c60ca50
FloNeu <di.neumann.florian@gmail.com>
2020-02-19 11:32:30 +0100

    Rework lesson 1 (2nd day)

--- src/app/modules/lesson1/lesson1.component.ts
+++ src/app/modules/lesson1/lesson1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { timer, interval } from 'rxjs';
 
 @Component({
   selector: 'app-lesson1',
@@ -7,12 +8,18 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class Lesson1Component implements OnInit {
   // Class-Member variables
-  public switchedName: string = '';
-  public showName( name: string ): void {
-    this.switchedName = name;
+  public switchedName: { name: string, status: boolean } = { name: 'FRITZ', status: false };
+  public showName( switched: {name: string, status: boolean} ): void {
+    this.switchedName.name = switched.name;
+    this.switchedName.status = switched.status;
   }
-
-  public ngOnInit(): void {}
+  public timer:boolean = true;
+  public ngOnInit(): void {
+    timer( 500 ).subscribe( tick => {
+      console.log('tick');
+      this.timer = !this.timer;
+    })
+  }
 
 }
 
