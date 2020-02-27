686f17654d0e4fe7906285c81c2671e9113ef730
josef.firmberger@agrardata.at <josef.firmberger@agrardata.at>
2020-02-27 08:56:48 +0100

    josef exercise 1

--- src/app/modules/exercise1/exercise1.component.ts
+++ src/app/modules/exercise1/exercise1.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercise1Component implements OnInit {
 
+  public switchedToggled: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    console.log('OnInit');
   }
 
+  public josef(toggle: { toggled: boolean }): void {
+    this.switchedToggled = toggle.toggled;
+    console.log('josef: ' + this.switchedToggled);
+  }
 }
--- src/app/modules/exercise1/exercise1.module.ts
+++ src/app/modules/exercise1/exercise1.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { Exercise1RoutingModule } from './exercise1-routing.module';
 import { Exercise1Component } from './exercise1.component';
+import { AppJosefComponent } from './app-josef/app-josef.component';
 
 
 @NgModule({
-  declarations: [Exercise1Component],
+  declarations: [Exercise1Component, AppJosefComponent],
   imports: [
     CommonModule,
     Exercise1RoutingModule
   ]
 })
-export class Exercise1Module { }
+export class Exercise1Module {
+
+}
